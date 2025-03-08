18610266015c335310deea976a0c38954f8dd190
Aizaj Samani <aizaj506@gmail.com>
2025-03-08 11:41:09 +0530

    Add bcrypt and jwt in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import { stringify } from "postcss";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new Schema({
     username: {
@@ -51,5 +53,41 @@ const userSchema = new Schema({
         type: String
     }
 }, { timestamps: true });
+
+// Hash password before saving to DB
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next(); //If Password is not modified
+    this.password = bcrypt.hash(this.password, 10)
+    next();
+})
+
+// Compare hashed password for login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+// Json Web Token (JWT)
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () { 
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+}
 
 export const User = mongoose.model("User", userSchema);
